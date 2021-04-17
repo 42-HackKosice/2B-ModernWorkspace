c30859909188a6ca75f9ce136c8c34eeaf0d0223
MatejMa2ur <matejmazur@outlook.com>
2021-04-17 17:15:58 +0200

    NotCleanUpdate

--- Canban/Data/Migrations/20210417151019_Initial-Create.Designer.cs
+++ Canban/Data/Migrations/20210417151019_Initial-Create.Designer.cs
@@ -4,14 +4,16 @@ using Canban.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Canban.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210417151019_Initial-Create")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Canban/Data/Migrations/20210417151019_Initial-Create.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Canban.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkGroup",
+                columns: table => new
+                {
+                    WorkGroupID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkGroup", x => x.WorkGroupID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    TaskID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TaskDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkGroupID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.TaskID);
+                    table.ForeignKey(
+                        name: "FK_Task_WorkGroup_WorkGroupID",
+                        column: x => x.WorkGroupID,
+                        principalTable: "WorkGroup",
+                        principalColumn: "WorkGroupID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypesOfBucket",
+                columns: table => new
+                {
+                    TypeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    workGroupID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypesOfBucket", x => x.TypeID);
+                    table.ForeignKey(
+                        name: "FK_TypesOfBucket_WorkGroup_workGroupID",
+                        column: x => x.workGroupID,
+                        principalTable: "WorkGroup",
+                        principalColumn: "WorkGroupID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_WorkGroupID",
+                table: "Task",
+                column: "WorkGroupID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TypesOfBucket_workGroupID",
+                table: "TypesOfBucket",
+                column: "workGroupID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+
+            migrationBuilder.DropTable(
+                name: "TypesOfBucket");
+
+            migrationBuilder.DropTable(
+                name: "WorkGroup");
+        }
+    }
+}
